e6bf4deb292a47a6327db08a58c6cc383d1cc2e8
Dhruv Gandhi <133976180+Dhruvgandhi29@users.noreply.github.com>
2023-11-22 20:16:27 +0530

    Update2 Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,10 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      //<h6 className="mb-2">Expense Management App</h6>
-      //<p className="text-muted" style={{ fontSize: "0.8em" }}>
-        //© 2023 All rights reserved
-      //</p>
+   
     </div>
   );
 };
