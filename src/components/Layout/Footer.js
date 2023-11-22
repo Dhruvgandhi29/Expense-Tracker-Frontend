7bf2cf424ba1f6a8294cc06c54b3993503f9eab4
Dhruv Gandhi <133976180+Dhruvgandhi29@users.noreply.github.com>
2023-11-22 20:14:26 +0530

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,14 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div
-      className="bg-dark text-light p-2 text-center"
-      style={{ position: "absolute", bottom: 0, width: "100%", zIndex: 1 }}
-    >
-      <h6 className="mb-2">Expense Management App</h6>
-      <p className="text-muted" style={{ fontSize: "0.8em" }}>
-        © 2023 All rights reserved
-      </p>
+    <div>
+      //<h6 className="mb-2">Expense Management App</h6>
+      //<p className="text-muted" style={{ fontSize: "0.8em" }}>
+        //© 2023 All rights reserved
+      //</p>
     </div>
   );
 };
